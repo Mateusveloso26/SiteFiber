7439afb70954427bedb67a3e53a49efcd7f253d9
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-07-02 16:58:54 -0300

    Remoção do botao de voltar e aumento do header

--- js/script.js
+++ js/script.js
@@ -43,18 +43,4 @@ window.onscroll = function () {
   scrollFunction()
 }
 
-// BOTAO VOLTAR AO TOPO
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('btnTop').style.display = 'block'
-  } else {
-    document.getElementById('btnTop').style.display = 'none'
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0
-  document.documentElement.scrollTop = 0
-}
-
 
